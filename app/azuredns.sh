e2a5b75494605843032a009ab1360744e7859291
grahamcropley <graham.cropley@gmail.com>
2025-02-21 22:31:04 +0000

    Update azuredns.sh

--- app/azuredns.sh
+++ app/azuredns.sh
@@ -1,7 +1,9 @@
-appId=""
-appSecret=""
-tenantId=""
-rgName=""
-zoneName=""
+clientId="$AZURE_CLIENTID"
+clientSecret="$AZURE_CLIENTSECRET"
+tenantId="$AZURE_TENANTID"
+resourceGroup="$AZURE_RESOURCEGROUP"
+cname=`cut -d "." -f 1 <<< "$AZURE_CNAME"`
+dnsZoneName=`cut -d "." -f 2- <<< "$AZURE_CNAME"`
 
-az login --service-principal --tenant $tenantId --username $appId --password $appSecret >/dev/null 2>&1
+az login --service-principal --tenant $tenantId --username $clientId --password $clientSecret >/dev/null 2>&1
+az network dns record-set cname set-record --resource-group $resourceGroup --zone-name dnsZoneName --record-set-name $cname
