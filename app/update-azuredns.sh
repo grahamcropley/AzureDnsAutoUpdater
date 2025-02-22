decade287b5c4f958090f3a7e10a06833e44b472
grahamcropley <graham.cropley@gmail.com>
2025-02-22 02:17:01 +0000

    Update update-azuredns.sh

--- app/update-azuredns.sh
+++ app/update-azuredns.sh
@@ -13,7 +13,7 @@ ERR=$(tput bold setaf 1)
 NORMAL=$(tput sgr0)
 
 az login --service-principal --tenant $tenantId --username $clientId --password $clientSecret > /dev/null 2>&1
-echo "${NORMAL}Azure CLI Login ${OK}Successul${NORMAL}"
+echo "${NORMAL}Azure CLI Login ${OK}Successul"
 
 if [ ! -f "$zoneFile" ]; then
   echo "${ERR}Zone File Does Not Exist${NORMAL}"
@@ -23,19 +23,19 @@ fi
 while IFS= read -r domain; do
   
   if [ ! -z $domain ]; then
-    echo -n "${NORMAL}Processing: ${INFO}$domain${NORMAL}"
+    echo -n "${NORMAL}Processing: ${INFO}$domain"
     recordSetName=`cut -d "." -f 1 <<< "$domain" | tr -d '\r' | tr -d '\n'`
     dnsZoneName=`cut -d "." -f 2- <<< "$domain" | tr -d '\r' | tr -d '\n'`
     az network dns record-set cname show --resource-group $resourceGroup --zone-name $dnsZoneName --name $recordSetName > /dev/null 2>&1
     if [ $? -ne 0 ]; then
       az network dns record-set cname set-record --resource-group $resourceGroup --zone-name $dnsZoneName --record-set-name $recordSetName --cname $dnsZoneName > /dev/null 2>&1
       if [ $? -eq 0 ]; then
-        echo -ne " CNAME ${OK}created${NORMAL}\n"
+        echo -ne "${NORMAL} CNAME ${OK}created${NORMAL}\n"
       else
-        echo -ne " CNAME ${ERR}failed${NORMAL}\n"
+        echo -ne "${NORMAL} CNAME ${ERR}failed${NORMAL}\n"
       fi
     else
-      echo -ne " CNAME ${WARN}already exists${NORMAL}\n"
+      echo -ne "${NORMAL} CNAME ${WARN}already exists${NORMAL}\n"
     fi
   fi
 done < "$zoneFile"
