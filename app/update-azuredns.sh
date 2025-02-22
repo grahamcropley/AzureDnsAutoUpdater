5e16b09febe500de09b08e9813598b19bb5e9639
grahamcropley <graham.cropley@gmail.com>
2025-02-22 00:38:54 +0000

    Update update-azuredns.sh

--- app/update-azuredns.sh
+++ app/update-azuredns.sh
@@ -4,8 +4,19 @@ clientId="$AZURE_CLIENTID"
 clientSecret="$AZURE_CLIENTSECRET"
 tenantId="$AZURE_TENANTID"
 resourceGroup="$AZURE_RESOURCEGROUP"
-cname=`cut -d "." -f 1 <<< "$AZURE_CNAME"`
-dnsZoneName=`cut -d "." -f 2- <<< "$AZURE_CNAME"`
+zoneFile="azuredns.zone"
 
 az login --service-principal --tenant $tenantId --username $clientId --password $clientSecret >/dev/null 2>&1
-az network dns record-set cname set-record --resource-group $resourceGroup --zone-name dnsZoneName --record-set-name $cname
+echo "Azure CLI Login Complete"
+
+if [ ! -f "$zoneFile" ]; then
+  echo "Zone File Does Not Exist"
+  exit 1
+fi
+
+while IFS= read -r domain; do
+  echo "Processing: $domain"
+  recordSetName=`cut -d "." -f 1 <<< "$domain"`
+  dnsZoneName=`cut -d "." -f 2- <<< "$domain"`
+  az network dns record-set cname set-record --resource-group $resourceGroup --zone-name dnsZoneName --record-set-name $recordSetName
+done < "$zoneFile"
