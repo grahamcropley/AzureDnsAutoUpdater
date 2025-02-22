5257d33686f8d6ffb6d0e8a35fab9220485c2bb2
grahamcropley <graham.cropley@gmail.com>
2025-02-22 00:50:56 +0000

    Update update-azuredns.sh

--- app/update-azuredns.sh
+++ app/update-azuredns.sh
@@ -15,8 +15,17 @@ if [ ! -f "$zoneFile" ]; then
 fi
 
 while IFS= read -r domain; do
-  echo "Processing: $domain"
-  recordSetName=`cut -d "." -f 1 <<< "$domain"`
-  dnsZoneName=`cut -d "." -f 2- <<< "$domain"`
-  az network dns record-set cname set-record --resource-group $resourceGroup --zone-name dnsZoneName --record-set-name $recordSetName
+  if [ ! -z $domain ]; then
+    echo "Processing: $domain"
+    recordSetName=`cut -d "." -f 1 <<< "$domain"`
+    dnsZoneName=`cut -d "." -f 2- <<< "$domain"`
+    az network dns record-set cname show --resource-group grahamcropley_lab --zone-name $dnsZoneName --name $recordSetName > /dev/null 2>&1
+    if [ $? -ne 0]; then
+      az network dns record-set cname set-record --resource-group $resourceGroup --zone-name $dnsZoneName --record-set-name $recordSetName > /dev/null 2>&1
+      if [ $? -eq 0]; then
+        echo "CNAME record created successfully for $domain"
+    else
+      echo "CNAM record already exists for $domain"
+    fi
+  fi
 done < "$zoneFile"
