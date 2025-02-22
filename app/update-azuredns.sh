fcfada17a624ba8dab36b66db23c864a7d759122
grahamcropley <graham.cropley@gmail.com>
2025-02-22 02:06:05 +0000

    Update update-azuredns.sh

--- app/update-azuredns.sh
+++ app/update-azuredns.sh
@@ -23,7 +23,7 @@ fi
 while IFS= read -r domain; do
   
   if [ ! -z $domain ]; then
-    echo "${$NORMAL}Processing: ${INFO}$domain${NORMAL}"
+    echo "${NORMAL}Processing: ${INFO}$domain${NORMAL}"
     recordSetName=`cut -d "." -f 1 <<< "$domain" | tr -d '\r' | tr -d '\n'`
     dnsZoneName=`cut -d "." -f 2- <<< "$domain" | tr -d '\r' | tr -d '\n'`
     az network dns record-set cname show --resource-group $resourceGroup --zone-name $dnsZoneName --name $recordSetName > /dev/null 2>&1
