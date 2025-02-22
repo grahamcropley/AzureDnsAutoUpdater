400eb1ce573104a4d24db007fefe17517c1c53f6
grahamcropley <graham.cropley@gmail.com>
2025-02-22 01:49:28 +0000

    Update update-azuredns.sh

--- app/update-azuredns.sh
+++ app/update-azuredns.sh
@@ -6,7 +6,7 @@ tenantId="$AZURE_TENANTID"
 resourceGroup="$AZURE_RESOURCEGROUP"
 zoneFile="azuredns.zone"
 
-az login --service-principal --tenant $tenantId --username $clientId --password $clientSecret
+az login --service-principal --tenant $tenantId --username $clientId --password $clientSecret > /dev/null 2>&1
 echo "Azure CLI Login Complete"
 
 if [ ! -f "$zoneFile" ]; then
@@ -20,13 +20,13 @@ while IFS= read -r domain; do
     echo "Processing: $domain"
     recordSetName=`cut -d "." -f 1 <<< "$domain" | tr -d '\r' | tr -d '\n'`
     dnsZoneName=`cut -d "." -f 2- <<< "$domain" | tr -d '\r' | tr -d '\n'`
-    az network dns record-set cname show --resource-group grahamcropley_lab --zone-name $dnsZoneName --name $recordSetName
+    az network dns record-set cname show --resource-group grahamcropley_lab --zone-name $dnsZoneName --name $recordSetName > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-      az network dns record-set cname set-record --resource-group $resourceGroup --zone-name $dnsZoneName --record-set-name $recordSetName --cname $dnsZoneName
+      az network dns record-set cname set-record --resource-group $resourceGroup --zone-name $dnsZoneName --record-set-name $recordSetName --cname $dnsZoneName > /dev/null 2>&1
       if [ $? -eq 0 ]; then
-        echo "CNAME record created successfully for $domain"
+        echo "   CNAME record created successfully for $domain"
       else
-        echo "CNAM record already exists for $domain"
+        echo "   CNAME record already exists for $domain"
       fi
     fi
   fi
